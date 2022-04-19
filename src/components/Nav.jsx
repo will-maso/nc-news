0fc7fb122906a2e66d8f45cbaffac81c80859a06
will-maso <ww=m4705211@gmail.com>
2022-04-19 12:46:10 +0100

    all done i hope

---
+++ src/components/Nav.jsx
@@ -0,0 +1,25 @@
+import axios from "axios"
+import { useEffect, useState } from "react"
+import {Link} from "react-router-dom"
+
+const Nav = () => {
+    const [topics, setTopics] = useState([]);
+    useEffect(() => {
+        axios.get("https://news-williammason.herokuapp.com/api/topics").then((response) => {
+            setTopics(response.data.topics)
+        }).catch((err) => {
+            console.log(err)
+        })
+    }, [])
+    return <section>
+        <h2>Nav</h2>
+        <nav>
+            <Link to="/">Home</Link>
+            {topics.map((topic) => {
+                return <Link to={`/${topic.slug}`} key={topic.slug}>{topic.slug}</Link>
+            })}
+        </nav>
+    </section>
+}
+
+export default Nav
