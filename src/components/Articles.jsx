1a670bef8b23d97dc57bdb5622d292ef96eb0171
will-maso <ww=m4705211@gmail.com>
2022-04-20 16:41:42 +0100

    sortby done

--- src/components/Articles.jsx
+++ src/components/Articles.jsx
@@ -5,14 +5,16 @@ import {Link} from "react-router-dom"
 const Articles = () => {
     const [articles, setArticles] = useState([])
     const [page, setPage] = useState(0)
+    const [sortby, setSortby] = useState("created_at")
+    const [orderby, setOrderby] = useState("desc")
     
     useEffect(() => {
-        axios.get(`https://news-williammason.herokuapp.com/api/articles?page=${page}`).then((data) => {
+        axios.get(`https://news-williammason.herokuapp.com/api/articles?page=${page}&&sort_by=${sortby}&&order=${orderby}`).then((data) => {
             setArticles(data.data.articles)
         }).catch((err) => {
             console.log(err)
         })
-    }, [page])
+    }, [page, sortby, orderby])
 
     const nextHandler = () => {
         setPage((currPage) => {
@@ -24,9 +26,26 @@ const Articles = () => {
             return currPage - 1
         })
     }
-
+    const sortHandler = (e) => {
+        setSortby(e.target.value)
+    }
+    const orderHandler = (e) => {
+        setOrderby(e.target.value)
+    }
     return (
         <main>
+            <br></br>
+            <label htmlFor="sortby">Choose filter: </label>
+            <select name="sortby" id="sortby" onChange={sortHandler}>
+                <option value="created_at">date</option>
+                <option value="votes">votes</option>
+                <option value="title">title</option>
+                <option value="comment_count">comment count</option>
+            </select>
+            <select name="orderby" id="orderby" onChange={orderHandler}>
+                <option value="desc">descending</option>
+                <option value="asc">ascending</option>
+            </select>
     <h2 className="title">Articles</h2>
     <p>Current Page: {page + 1}</p>
     {page > 0 && <button onClick={prevHandler}>Previous Page</button>} 
@@ -36,6 +55,7 @@ const Articles = () => {
             return <li key={article.article_id}>
                 <Link to={`/articles/${article.article_id}` } >
                 Topic: {article.topic}.  "{article.title}"
+                Comments: {article.comment_count}
                 </Link>
             </li>
         })}
--- src/components/SingleTopic.jsx
+++ src/components/SingleTopic.jsx
@@ -6,14 +6,16 @@ const SingleTopic = () => {
 const [articles, setArticles] = useState([])
 const {topic_slug} = useParams()
 const [page, setPage] = useState(0)
+const [sortby, setSortby] = useState("created_at")
+const [orderby, setOrderby] = useState("desc")
 
     useEffect(() => {
-        axios.get(`https://news-williammason.herokuapp.com/api/articles/?topic=${topic_slug}&&page=${page}`).then((data) => {
+        axios.get(`https://news-williammason.herokuapp.com/api/articles/?topic=${topic_slug}&&page=${page}&&sort_by=${sortby}&&order=${orderby}`).then((data) => {
             setArticles(data.data.articles)
         }).catch((err) => {
             console.log(err.response)
         })
-    }, [topic_slug, page])
+    }, [topic_slug, page, sortby, orderby])
     const nextHandler = () => {
         setPage((currPage) => {
             return currPage + 1
@@ -24,8 +26,27 @@ const [page, setPage] = useState(0)
             return currPage - 1
         })
     }
+    const sortHandler = (e) => {
+        setSortby(e.target.value)
+    }
+    const orderHandler = (e) => {
+        setOrderby(e.target.value)
+    }
+
     return (
         <main>
+            <br></br>
+            <label htmlFor="sortby">Choose filter: </label>
+            <select name="sortby" id="sortby" onChange={sortHandler}>
+                <option value="created_at">date</option>
+                <option value="votes">votes</option>
+                <option value="title">title</option>
+                <option value="comment_count">comment count</option>
+            </select>
+            <select name="orderby" id="orderby" onChange={orderHandler}>
+                <option value="desc">descending</option>
+                <option value="asc">ascending</option>
+            </select>
     <h2 className="title">Articles</h2>
     <p>Current Page: {page + 1}</p>
     {page > 0 && <button onClick={prevHandler}>Previous Page</button>} 
@@ -36,6 +57,7 @@ const [page, setPage] = useState(0)
             <li key={article.article_id}>
                 <Link to={`/articles/${article.article_id}`}>
                 Topic: {article.topic}.  "{article.title}"
+                Comments: {article.comment_count}
                 </Link>
             </li>
             )
